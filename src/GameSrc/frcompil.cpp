7fcf7a92a6e0e948ee01810cfcc6847ab4e639f5
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2021-08-24 23:36:48 +0300

    Remove last of __*_SRC defines

--- src/GameSrc/frcompil.cpp
+++ src/GameSrc/frcompil.cpp
@@ -36,7 +36,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * Initial revision
  *
  */
-#define __FRCOMPIL_SRC
 #include "frintern.h"
 #include "map.h"
 #include "mapflags.h"
